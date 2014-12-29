a1395c1c53752f45d7fd886c314edd026ba2fafb
Rudy Crespo <rudy.crespo20@gmail.com>
2014-12-28 22:10:30 -0500

    anchor scroll

---
+++ app/scripts/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var anchorScroll = (function() {
+	var s;
+
+	return {
+
+		settings: {
+			navLink: $('.anchor-link')
+		},
+
+		init: function() {
+			s = this.settings;
+			this.scrollToSection();
+		},
+
+		scrollToSection: function() {
+			s.navLink.on('click', function(e) {
+				var linkId = this.hash;
+				e.preventDefault();
+
+				$(linkId).velocity('scroll', { duration: 750, offset: -75});
+			});
+		}
+
+	};
+
+
+}());
+
+
+$(function() {
+	anchorScroll.init();
+});
